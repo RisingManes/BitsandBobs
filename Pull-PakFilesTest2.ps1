94ac7d41efbe1fe30d76e6853bfb4db15cce4e1e
RisingManes <shadowlove16@gmail.com>
2021-03-19 00:29:17 -0400

    Add files via upload

--- Pull-PakFilesTest2.ps1
+++ Pull-PakFilesTest2.ps1
@@ -9,16 +9,18 @@ param (
 
 # Will be replaced by an input field. # Read-Host -Prompt 'Input source name' #
 $sourcepath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\211820"
-# Output folder creation and assignation
-New-Item -path $sourcepath -ItemType directory -Name Output
+# Output folder creation and assignation. Code by Randych
+if (-not (Test-Path -Path $sourcepath)) {
+    New-Item -path $sourcepath -ItemType directory -Name Output
+    }
+    $ExcludeFolders = "output|pictures|photos|Screenshot"
 $outputfolder = "$sourcepath\Output"
-# Getting the folder names, with any luck. Should assign them as "foldername"
-$foldername = Get-ChildItem $sourcepath -Recurse
+# Getting the folder names, with any luck. Should assign them as "foldername" $foldername = 
+Get-ChildItem $sourcepath -directory | Where-Object Fullname -notmatch $ExcludeFolders # !!!why does this line still show me the output folder, anyway?!!!
 #I should also get the actual contents. "contents"
-$contents = Get-ChildItem $foldername -Recurse
+Get-ChildItem $sourcepath -recurse -exclude $outputfolder
 #Now to extract the numbers from this list. Will be "modid"
-$modid = Get-ChildItem $sourcepath -recurse -Exclude Output
-#okay, so I guess I can't just do what I did in that lasy part.
-$composite = $outputfolder+'\'+$modid+'.pak'
+Get-ChildItem $sourcepath -Exclude Output | Tee-Object -variable modid
+$composite = write-output $outputfolder\$modid+'.pak'
 #maybe this last part will work.
-#write-host Copy-Item -path $contents -destination "$composite" -recurse
+Write-Host Copy-Item $ProcessTheseFiles -Destination $outputfolder -force
