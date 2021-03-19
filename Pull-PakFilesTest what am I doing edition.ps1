fb73e293349eb1ca26ebff9b0713d12b49bc4b0d
RisingManes <shadowlove16@gmail.com>
2021-03-19 01:46:44 -0400

    Add files via upload

--- Pull-PakFilesTest what am I doing edition.ps1
+++ Pull-PakFilesTest what am I doing edition.ps1
@@ -3,10 +3,11 @@ $sourcepath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\211820"
 # Output folder creation and assignation.
 $outputfolder = "$sourcepath\Output"
 # directory: has folders, which contain mod ids.
-Get-ChildItem -path $sourcepath | Tee-Object  -Variable directory 
+Get-ChildItem -path $sourcepath -exclude $outputfolder| Tee-Object  -Variable directory 
 # contents: what's inside the actual mod folders.
 Get-ChildItem -path $directory | Tee-Object -variable contents
-get-childitem -name $sourcepath | tee-object -Variable modid
-# Now to extract the numbers from this list. Will be "modid"
+get-childitem -name $sourcepath -recurse | tee-object -Variable modid
+# Now to extract the numbers from this list. Will be "modid" 
 #maybe this last part will work.
-Copy-Item -path $contents -Destination $outputfolder/$modid.pak
+Write-Host $outputfolder+'\'+$modid
+# Copy-Item -path $contents -Destination $outputfolder
